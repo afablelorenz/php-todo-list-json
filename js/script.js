865a18987a5df395f01ff60cccd5f5833e5c38b0
afablelorenz <afablelorenz@gmail.com>
2024-07-01 23:47:51 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,12 +4,24 @@ createApp({
 
     data() {
         return {
-
+            toDoTasks: [],
+            newTask: ''m
         }
     },
-
+    created(){
+        this.getTasks();
+    },
     methods: {
-
+        getTasks(){
+            axios.get('../json/toDoList.json')
+                .then(response =>{
+                    this.tasks = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        
     }
 
 }).mount('#app')
